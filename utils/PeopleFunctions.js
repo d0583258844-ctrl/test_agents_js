ee19c31782ddaf0f92f946d54e42c880b7bc5714
Daniel <d0583258844@gmail.com>
2025-12-17 12:44:41 +0200

    I added function of searching by age

--- utils/PeopleFunctions.js
+++ utils/PeopleFunctions.js
@@ -23,3 +23,16 @@ export async function SearchPeopleByName(Name, path) {
   }
   console.log("User name not found!");
 }
+
+export async function SearchPeoplebyAge(Age, path) {
+  const data = await fs.readFile(path, "utf-8");
+  const data1 = JSON.parse(data);
+  for (let i = 0; i < data1.length; i++) {
+    const element = data1[i];
+    if (element["age"] === Age) {
+      console.log(element);
+      return;
+    }
+  }
+  console.log("User age not found!");
+}
